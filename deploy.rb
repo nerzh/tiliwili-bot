e12b36f94944b70e24b36155a4cc10f6ef65c23f
nerzh <emptystamp@gmail.com>
2023-03-11 05:53:19 +0100

    maybe fix

--- deploy.rb
+++ deploy.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 ssh_key_name = "bot.bytehub.io"
-system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{ssh_key_name}.pub || true && git reset --hard HEAD || true && git pull origin master")
+system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{ssh_key_name}.pub || true && ssh-add ~/.ssh/#{ssh_key_name} || true && git reset --hard HEAD || true && git pull origin master")
 system("swift package resolve")
 #system("swift package update")
 system("swift build -c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization -Xcc -O2")
