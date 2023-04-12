ff4a125c88b93d39a60485962f01f0bc3d3a4431
nerzh <emptystamp@gmail.com>
2023-04-13 00:21:38 +0200

    delete

--- deploy.rb
+++ deploy.rb
@@ -7,8 +7,6 @@ system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{ssh_key_name}.pub |
 system("swift package resolve")
 #system("swift package update")
 system("swift build -c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization -Xcc -O2")
-#system("swift build -c debug")
-system("cd #{current_file_path}/get_congig_params && npm install")
 system("sudo pkill -9 -f #{app_name}")
 # system("/home/devton/swift/#{name}/.build/release/#{name} --env production > /home/devton/swift/#{name}/log.txt 2>&1 &")
 system("sudo supervisorctl restart #{app_name}-app")
