9fc6bb02397c5cf482881cac1c74860b39a7604c
nerzh <emptystamp@gmail.com>
2025-09-27 17:13:42 +0200

    fix deploy

--- deploy.rb
+++ deploy.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
 current_file_path = File.expand_path(File.dirname(__FILE__))
-ssh_key_name = "botasbot.bytehub.io"
+ssh_key_name = "tiliwili"
 app_name = "tiliwili"
 system("eval \"$(ssh-agent -s)\" || true && ssh-add ~/.ssh/#{ssh_key_name}.pub || true && ssh-add ~/.ssh/#{ssh_key_name} || true && git reset --hard HEAD || true && git pull origin master")
-#system("swift package resolve")
-system("swift package update")
-system("swift build -c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization -Xcc -O2")
-system("sudo systemctl start #{app_name}")
-system("sudo pkill -9 -f #{app_name}")
-system("sudo systemctl start #{app_name}")
+system("swift package resolve")
+# system("swift package update")
+system("swift build -c release -Xswiftc -Ounchecked -Xcc -O2")
+system("sudo systemctl stop #{app_name}")
+system("sudo systemctl restart #{app_name}")
 # system("/home/devton/swift/#{name}/.build/release/#{name} --env production > /home/devton/swift/#{name}/log.txt 2>&1 &")
